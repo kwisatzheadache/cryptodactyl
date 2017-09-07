eae58bc605b344db947cff732ae9a24e2742eae2
kwisatzheadache <michaelhorvath87@gmail.com>
2017-09-07 14:55:24 -0500

    data cleaned and merged

--- import.py
+++ import.py
@@ -1,20 +1,13 @@
-from pandas import Series
 from pandas import read_csv
-from pandas import concat
-from matplotlib import pyplot
-from pandas import DataFrame
-import pandas as pd
-# import keras
-# import tensorflow
+from pandas import merge
 
-
-btc = read_csv('./bitcoin_price.csv')
-dash = read_csv('./dash_price.csv', header=0)
-eth = read_csv('ethereum_price.csv', header=0)
-ethc = read_csv('ethereum_classic_price.csv', header=0)
-ltc = read_csv('litecoin_price.csv', header=0)
-monero = read_csv('monero_price.csv', header=0)
-ripple = read_csv('ripple_price.csv', header=0)
+btc = read_csv('./data/used/bitcoin_price.csv')
+dash = read_csv('./data/used/dash_price.csv', header=0)
+eth = read_csv('./data/used/ethereum_price.csv', header=0)
+ethc = read_csv('./data/used/ethereum_classic_price.csv', header=0)
+ltc = read_csv('./data/used/litecoin_price.csv', header=0)
+monero = read_csv('./data/used/monero_price.csv', header=0)
+ripple = read_csv('./data/used/ripple_price.csv', header=0)
 
 coin_list = [btc, dash, eth, ethc, ltc, monero, ripple]
 coin_names = ['btc', 'dash', 'eth', 'ethc', 'ltc', 'monero', 'ripple']
@@ -40,7 +33,7 @@ for i in range(len(coin_list)):
 
 
 # Combine coins into one large df
-merged = reduce(lambda left,right: pd.merge(left,right,on='Date'), coin_list)
+merged = reduce(lambda left,right: merge(left,right,on='Date'), coin_list)
 
-
+print('variable "merged" contains 409 days worth of market info for seven coins.')
 
