30f6b5f14bd8b27bc482b296b00eeef863d681d9
kwisatzheadache <michaelhorvath87@gmail.com>
2017-09-22 15:35:25 -0700

    lag created, but with NANs present

--- analysis.py
+++ analysis.py
@@ -2,8 +2,6 @@ from pandas import read_csv
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-lagged = DataFrame()
 for i in merged.columns:
-    column = merged[i]
-    lagged = lagged.append(lag(column, 7))
-
+    lagged = lag(merged[i], 7)
+    merged[lagged.columns] = lagged
